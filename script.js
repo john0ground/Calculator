2c957614af028a2bf7e5019bbd4593492142b834
John <mjohncorbet@gmail.com>
2022-11-29 20:49:38 +0800

    create operator

---
+++ script.js
@@ -0,0 +1,22 @@
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+let operator = 'divide';
+
+function operate(x, y) {
+    switch (operator) {
+        case 'add':
+            return add(x, y);
+        case 'subtract':
+            return subtract(x, y);
+        case 'multiply':
+            return multiply(x, y);
+        case 'divide':
+            return divide(x, y);
+    }
+}
